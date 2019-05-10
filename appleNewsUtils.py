8220a1127bc6e9f32965eec34640ae0ff2922605
naturale0 <taran_sh@korea.ac.kr>
2019-05-10 14:58:00 +0900

    Delete appleNewsUtils.py

--- appleNewsUtils.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def get_redirect_url(apple_news_url):
-    resp = requests.get(apple_news_url)
-    soup = BeautifulSoup(resp.text, 'html5lib')
-    redirect_url = soup.find_all('a', href=True)[0]["href"]
-    return redirect_url
-
